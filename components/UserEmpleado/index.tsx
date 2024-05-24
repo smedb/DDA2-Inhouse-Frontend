97d5ac35a5f44da00791161227aa2b25b46b5915
pedromoura1 <55463700+pedromoura1@users.noreply.github.com>
2024-05-24 10:20:26 -0300

    page routing + logged user + logout (#13)

--- components/UserEmpleado/index.tsx
+++ components/UserEmpleado/index.tsx
@@ -1,272 +1,143 @@
 "use client";
-import { motion } from "framer-motion";
-import Image from "next/image";
 import React, { useState, useEffect } from "react";
+import { motion } from "framer-motion";
 import axios from "axios";
+import Image from "next/image";
+import Link from "next/link";
+
+// Define the Employee interface
+interface Employee {
+  _id: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  monthlySalary: number;
+  department: string;
+  age: number;
+  state: string;
+  gender: string;
+  birthDate: string;
+  __v: number;
+}
 
 const UserEmpleado = () => {
   const [hasMounted, setHasMounted] = useState(false);
-  const [formData, setFormData] = useState({
-    firstName: '',
-    lastName: '',
-    email: '',
-    password: '',
-    gender: '',
-    monthlySalary: '',
-    department: '',
-    age: '',
-    birthDate: '',
-  });
-  const [successMessage, setSuccessMessage] = useState('');
-  const [emailError, setEmailError] = useState(false);
+  const [employees, setEmployees] = useState<Employee[]>([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
     setHasMounted(true);
+
+    const fetchEmployees = async () => {
+      try {
+        const token = localStorage.getItem("authToken");
+        const response = await axios.get('https://api.inhouse.deliver.ar/users/employee', {
+          headers: {
+            Authorization: `${localStorage.getItem("authToken")}`,
+          },
+        });
+
+        if (response.data) {
+          setEmployees(response.data);
+        } else {
+          setError('API error');
+        }
+      } catch (error) {
+        setError('Usted no está autorizado.');
+      }
+    };
+
+    fetchEmployees();
   }, []);
+
+  const handleDelete = async (id: string) => {
+    try {
+      const token = localStorage.getItem("authToken");
+      await axios.delete(`https://api.inhouse.deliver.ar/users/employee/${id}`, {
+        headers: {
+          Authorization: `${localStorage.getItem("authToken")}`,
+        },
+      });
+
+      setEmployees(employees.filter(employee => employee._id !== id));
+    } catch (error) {
+      setError('Ops. Contacte a XWallet Help Desk y comparta el id: 0803');
+    }
+  };
 
   if (!hasMounted) {
     return null;
   }
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setFormData({ ...formData, [name]: value });
-  };
-
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    try {
-      const response = await axios.post('https://api.inhouse.deliver.ar/users/employee', {
-        users: [
-          {
-            ...formData,
-            state: "state" // Assuming 'state' is a required field, you may modify or remove it as needed
-          },
-        ]
-      });
-      setSuccessMessage(`Usuario creado con éxito: ${response.data.users[0].email}`);
-      setEmailError(false);
-    } catch (error) {
-      if (error.response && error.response.status === 500) {
-        setEmailError(true);
-      } else {
-        console.error('Error:', error);
-      }
-    }
-  };
-
   return (
-    <>
-      {successMessage && (
-        <div className="fixed inset-0 z-50 flex items-center justify-center bg-black bg-opacity-50">
-          <div className="bg-white p-6 rounded shadow-lg">
-            <h2 className="text-xl font-semibold">¡Éxito!</h2>
-            <p>{successMessage}</p>
-            <button
-              onClick={() => setSuccessMessage('')}
-              className="mt-4 rounded bg-blue-500 px-4 py-2 text-white"
-            >
-              Cerrar
-            </button>
-          </div>
+    <section className="py-20 lg:py-25 xl:py-30">
+      <div className="mx-auto max-w-c-1315 px-4 md:px-8 xl:px-0">
+        <div className="animate_top mx-auto text-center">
+          <SectionHeader
+            headerInfo={{
+              title: `Employees`,
+              subtitle: `Summary of Employees`,
+              description: `The listed employees have access to XWallet Internal.`,
+            }}
+          />
         </div>
-      )}
-      <section id="support" className="px-4 md:px-8 2xl:px-0 flex items-center justify-center min-h-screen">
-        <div className="relative mx-auto max-w-c-1390 px-7.5 pt-10 lg:px-15 lg:pt-15 xl:px-20 xl:pt-20">
-          <div className="absolute left-0 top-0 -z-1 h-2/3 w-full rounded-lg bg-gradient-to-t from-transparent to-[#dee7ff47] dark:bg-gradient-to-t dark:to-[#252A42]"></div>
-          <div className="absolute bottom-[-255px] left-0 -z-1 h-full w-full">
-            <Image
-              src="./images/shape/shape-dotted-light.svg"
-              alt="Dotted"
-              className="dark:hidden"
-              fill
-            />
-            <Image
-              src="./images/shape/shape-dotted-dark.svg"
-              alt="Dotted"
-              className="hidden dark:block"
-              fill
-            />
-          </div>
+      </div>
 
-          <div className="flex flex-col-reverse flex-wrap gap-8 md:flex-row md:flex-nowrap md:justify-center xl:gap-20">
-            <motion.div
-              variants={{
-                hidden: {
-                  opacity: 0,
-                  y: -20,
-                },
-                visible: {
-                  opacity: 1,
-                  y: 0,
-                },
-              }}
-              initial="hidden"
-              whileInView="visible"
-              transition={{ duration: 1, delay: 0.1 }}
-              viewport={{ once: true }}
-              className="animate_top w-full rounded-lg bg-white p-7.5 shadow-solid-8 dark:border dark:border-strokedark dark:bg-black md:w-3/5 lg:w-3/4 xl:p-15"
-            >
-              <h2 className="mb-15 text-3xl font-semibold text-black dark:text-white xl:text-sectiontitle2">
-                Alta de empleado
-              </h2>
-
-              <form onSubmit={handleSubmit}>
-                <div className="mb-7.5 flex flex-col gap-7.5 lg:flex-row lg:justify-between lg:gap-14">
-                  <input
-                    type="text"
-                    name="firstName"
-                    placeholder="Nombre"
-                    value={formData.firstName}
-                    onChange={handleChange}
-                    className="w-full border-b border-stroke bg-transparent pb-3.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white lg:w-1/2"
-                  />
-
-                  <input
-                    type="text"
-                    name="lastName"
-                    placeholder="Apellido"
-                    value={formData.lastName}
-                    onChange={handleChange}
-                    className="w-full border-b border-stroke bg-transparent pb-3.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white lg:w-1/2"
-                  />
-                </div>
-
-                <div className="mb-7.5 flex flex-col gap-7.5 lg:flex-row lg:justify-between lg:gap-14">
-                  <div className="w-full lg:w-1/2">
-                    <input
-                      type="email"
-                      name="email"
-                      placeholder="Mail"
-                      value={formData.email}
-                      onChange={handleChange}
-                      className={`w-full border-b ${emailError ? 'border-red-500' : 'border-stroke'} bg-transparent pb-3.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white`}
-                    />
-                    {emailError && <p className="mt-3 text-xs text-red-500">*Mail ya registrado</p>}
-                  </div>
-
-                  <input
-                    type="password"
-                    name="password"
-                    placeholder="Clave"
-                    value={formData.password}
-                    onChange={handleChange}
-                    className="w-full border-b border-stroke bg-transparent pb-3.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white lg:w-1/2"
-                  />
-                </div>
-
-                <div className="mb-7.5 flex flex-col gap-7.5 lg:flex-row lg:justify-between lg:gap-14">
-                  <select
-                    name="gender"
-                    value={formData.gender}
-                    onChange={handleChange}
-                    className="w-full border-b border-stroke bg-transparent pb-3.5 focus:border-waterloo focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee lg:w-1/2"
-                  >
-                    <option value="" disabled>
-                      Sexo
-                    </option>
-                    <option value="M">Masculino</option>
-                    <option value="F">Femenino</option>
-                  </select>
-
-                  <input
-                    type="number"
-                    name="monthlySalary"
-                    placeholder="Sueldo"
-                    value={formData.monthlySalary}
-                    onChange={handleChange}
-                    className="w-full border-b border-stroke bg-transparent pb-3.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white lg:w-1/2"
-                  />
-                </div>
-
-                <div className="mb-7.5 flex flex-col gap-7.5 lg:flex-row lg:justify-between lg:gap-14">
-                  <select
-                    name="department"
-                    value={formData.department}
-                    onChange={handleChange}
-                    className="w-full border-b border-stroke bg-transparent pb-3.5 focus:border-waterloo focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee lg:w-1/2"
-                  >
-                    <option value="" disabled>
-                      Departamento
-                    </option>
-                    <option value="Finance">Finance</option>
-                    <option value="Tech">Tech</option>
-                    <option value="Client Services">Client Services</option>
-                    <option value="Admin">Admin</option>
-                  </select>
-
-                  <input
-                    type="date"
-                    name="birthDate"
-                    placeholder="Birth Date"
-                    value={formData.birthDate}
-                    onChange={handleChange}
-                    className="w-full border-b border-stroke bg-transparent pb-3.5 focus:border-waterloo focus:placeholder:text-black focus-visible:outline-none dark:border-strokedark dark:focus:border-manatee dark:focus:placeholder:text-white lg:w-1/2"
-                  />
-                </div>
-
-                <div className="flex flex-wrap gap-4 xl:justify-between">
-                  <div className="mb-4 flex md:mb-0">
-                    <input
-                      id="default-checkbox"
-                      type="checkbox"
-                      className="peer sr-only"
-                    />
-                    <span className="border-gray-300 bg-gray-100 text-blue-600 dark:border-gray-600 dark:bg-gray-700 group mt-2 flex h-5 min-w-[20px] items-center justify-center rounded peer-checked:bg-primary">
-                      <svg
-                        className="opacity-0 peer-checked:group-[]:opacity-100"
-                        width="10"
-                        height="8"
-                        viewBox="0 0 10 8"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          fillRule="evenodd"
-                          clipRule="evenodd"
-                          d="M9.70704 0.792787C9.89451 0.980314 9.99983 1.23462 9.99983 1.49979C9.99983 1.76495 9.89451 2.01926 9.70704 2.20679L4.70704 7.20679C4.51951 7.39426 4.26521 7.49957 4.00004 7.49957C3.73488 7.49957 3.48057 7.39426 3.29304 7.20679L0.293041 4.20679C0.110883 4.01818 0.0100885 3.76558 0.0123669 3.50339C0.0146453 3.24119 0.119814 2.99038 0.305222 2.80497C0.490631 2.61956 0.741443 2.51439 1.00364 2.51211C1.26584 2.50983 1.51844 2.61063 1.70704 2.79279L4.00004 5.08579L8.29304 0.792787C8.48057 0.605316 8.73488 0.5 9.00004 0.5C9.26521 0.5 9.51951 0.605316 9.70704 0.792787Z"
-                          fill="white"
-                        />
-                      </svg>
-                    </span>
-                    <label
-                      htmlFor="default-checkbox"
-                      className="flex max-w-[425px] cursor-pointer select-none pl-5"
-                    >
-                      Al hacer clic en esta casilla de verificación, confirmo que el nuevo empleado cumplió con el proceso de contratación y cumplimiento.
-                    </label>
-                  </div>
-
-                  <div className="flex justify-center w-full">
+      <div className="mx-auto mt-15 max-w-c-1280 px-4 md:px-8 xl:mt-20 xl:px-0">
+        {error && <p className="text-red-500">{error}</p>}
+        {employees.length === 0 ? (
+          <p>No se encontraron empleados.</p>
+        ) : (
+          <div className="grid grid-cols-1 gap-7.5 md:grid-cols-2 lg:grid-cols-3 xl:gap-10">
+            {employees.map((employee) => (
+              <motion.div
+                key={employee._id}
+                variants={{
+                  hidden: { opacity: 0, y: -20 },
+                  visible: { opacity: 1, y: 0 },
+                }}
+                initial="hidden"
+                whileInView="visible"
+                transition={{ duration: 1, delay: 0.5 }}
+                viewport={{ once: true }}
+                className="animate_top rounded-lg bg-white p-4 pb-9 shadow-solid-8 dark:bg-blacksection"
+              >
+                <div className="px-4">
+                  <h3 className="mb-3.5 mt-7.5 text-lg font-medium text-black dark:text-white xl:text-itemtitle2">
+                    {employee.firstName} {employee.lastName}
+                  </h3>
+                  <p className="text-sm text-gray-600 dark:text-gray-400">Email: {employee.email}</p>
+                  <p className="text-sm text-gray-600 dark:text-gray-400">Department: {employee.department}</p>
+                  <p className="text-sm text-gray-600 dark:text-gray-400">Salary: {employee.monthlySalary}</p>
+                  <p className="text-sm text-gray-600 dark:text-gray-400">Gender: {employee.gender}</p>
+                  <p className="text-sm text-gray-600 dark:text-gray-400">Birth Date: {new Date(employee.birthDate).toLocaleDateString()}</p>
+                  <div className="flex justify-center mt-4">
                     <button
-                      type="submit"
-                      aria-label="register user"
-                      className="inline-flex items-center gap-2.5 rounded-full bg-black px-6 py-3 font-medium text-white duration-300 ease-in-out hover:bg-blackho dark:bg-btndark"
+                      className="rounded bg-red-500 py-2 px-4 text-white hover:bg-red-700"
+                      onClick={() => handleDelete(employee._id)}
                     >
-                      Register User
-                      <svg
-                        className="fill-white"
-                        width="14"
-                        height="14"
-                        viewBox="0 0 14 14"
-                        fill="none"
-                        xmlns="http://www.w3.org/2000/svg"
-                      >
-                        <path
-                          d="M10.4767 6.16664L6.00668 1.69664L7.18501 0.518311L13.6667 6.99998L7.18501 13.4816L6.00668 12.3033L10.4767 7.83331H0.333344V6.16664H10.4767Z"
-                          fill=""
-                        />
-                      </svg>
+                      Eliminar acceso
                     </button>
                   </div>
                 </div>
-              </form>
-            </motion.div>
+              </motion.div>
+            ))}
           </div>
-        </div>
-      </section>
-    </>
+        )}
+      </div>
+    </section>
   );
 };
 
 export default UserEmpleado;
+
+// SectionHeader Component
+const SectionHeader = ({ headerInfo }: { headerInfo: { title: string; subtitle: string; description: string; } }) => {
+  return (
+    <div>
+      <h2 className="text-3xl font-semibold text-black dark:text-white">{headerInfo.title}</h2>
+      <p className="text-lg text-black dark:text-white">{headerInfo.subtitle}</p>
+      <p className="text-sm text-gray-600">{headerInfo.description}</p>
+    </div>
+  );
+};
